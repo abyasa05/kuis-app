25a0f4fbfa3e86add57ac1b01fdb2e55919a58a3
abyasa05 <abyprat07@gmail.com>
2026-06-12 00:06:04 +0700

    feat: add attempt & finish button

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -2,11 +2,17 @@
 import { getQuizQuestions } from "@/utils"
 import { useQuizStore } from "@/stores"
 import { useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
 
 export default function Quiz() {
-    const questions = useQuizStore((state) => state.questions)
-    const setQuestions = useQuizStore((state) => state.setQuestions)
-		const hasHydrated = useQuizStore((state) => state.hasHydrated)
+		const router = useRouter();
+    const questions = useQuizStore((state) => state.questions);
+    const setQuestions = useQuizStore((state) => state.setQuestions);
+		const hasHydrated = useQuizStore((state) => state.hasHydrated);
+		const currentNum = useQuizStore((state) => state.currentNum);
+		const isActive = useQuizStore((state) => state.isActive);
+		const setIsActive = useQuizStore((state) => state.setIsActive);
+		const setcurrentNum = useQuizStore((state) => state.setCurrentNum);
 		const [selectedItem, setSelectedItem] = useState("");
 
     useEffect(() => {
@@ -28,10 +34,16 @@ export default function Quiz() {
         }
     }
 
+		const handleFinish = () => {
+			setIsActive(false);
+			setQuestions(null);
+			router.push('/');
+		}
+
     return (
 			<div>
 				{
-					questions && (
+					questions && currentNum && (
 						<form>
 							<h3>{ questions[0].question }</h3>
 
@@ -52,5 +64,9 @@ export default function Quiz() {
 						</form>
 					)
 				}
+
+				<button onClick={handleFinish}>
+					Finish
+				</button>
 			</div>
 )}
